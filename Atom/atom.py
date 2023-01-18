f084e9171172957b2c89d3031e9546fa89c3d995
k0dsdev <cathating@protonmail.com>
2023-01-17 20:25:49 -0800

    finished

--- Atom/atom.py
+++ Atom/atom.py
@@ -23,6 +23,13 @@ class Atom(Scene):
         electrontext = Text("Electron -", font_size=20).next_to(elec1, DOWN)
         atomText = Text("Hydrogen Atom  H").next_to(orbit1, UP)
         bondText = Text("Covalent Bond, electrons are shared", color=YELLOW, font_size=25).next_to(atomText, DOWN)
+        bondText2 = Text("Shared electrons balance attraction and repulsion", color=YELLOW, font_size=25)
+        bondText3 = Text("This allows each atom to reach a full valence shell", color=YELLOW, font_size=25).next_to(bondText2, DOWN)
+       
+        bondtextG = VGroup(bondText2, bondText3).next_to(atomText, DOWN)
+        bondttext4 = Text("And reach stability.", color=ORANGE, font_size=30).to_edge(DOWN)
+       
+
 
         self.play(Create(proton1))
         self.play(Create(orbit1))
@@ -44,6 +51,12 @@ class Atom(Scene):
 
         atom2 = VGroup(proton2, orbit2, elec2)
 
+        atom1Center =  orbit1.get_center()
+        atom2Center = orbit1.get_center()
+
+        atomH = Text("H", color=WHITE, font_size=40).next_to(atom1, atom1Center)
+        atomH2 = Text("H", color=WHITE, font_size=40).next_to(atom2, atom2Center)
+
         self.play(Create(atom2))
         self.play(atom2.animate.shift(LEFT))
 
@@ -63,12 +76,41 @@ class Atom(Scene):
 
         self.play(atom1.animate.next_to(atom2, RIGHT), atom2.animate.next_to(atom1, LEFT))
 
-        self.play(elec2.animate.shift(LEFT))
+        self.play(elec2.animate.shift(LEFT / 2.5))
         self.play(elec1.animate.next_to(elec2, DOWN))
 
-        #figure out how to make elec2 shift to the left less, maybe through coordinates
+        electrons = VGroup(elec1, elec2)
+        self.play(electrons.animate.shift(UP / 4))
 
-        self.wait(5)
+        BondedAtoms = Group(atom1, atom2)
+
+        self.play(BondedAtoms.animate.shift(LEFT))
+
+        self.play(FadeOut(bondText))
+        self.play(atomText.animate.to_edge(UP))
+
+        self.play(FadeIn(bondText2))
+        self.play(FadeIn(bondText3))
+        self.play(bondtextG.animate.shift(UP/2))
+
+        
+        self.remove(proton1,proton2)
+        
+        self.play(atom1.animate.set_fill(color=BLUE, opacity=1))
+        self.play(atom2.animate.set_fill(color=BLUE, opacity=0.98))
+        self.add(orbit1.set_opacity(0.98))
+        self.add(orbit2.set_opacity(0.98))
+       
+        
+
+        self.add(atomH.next_to(orbit1.get_center()))
+        self.add(atomH2.next_to(orbit2.get_center()))
+        
+
+        self.play(FadeIn(bondttext4))
+
+        self.wait(10)
+
         
 
           
